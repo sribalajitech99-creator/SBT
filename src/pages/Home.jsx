ae98bf1f7a03f51ef111c834bf8a4a80a962a3af
sribalajitech99-creator <sribalajitech99@gmail.com>
2026-04-21 23:43:26 +0530

    Change

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -4,8 +4,9 @@ import { motion } from 'framer-motion';
 import { FiAward, FiTarget, FiClock, FiUsers } from 'react-icons/fi';
 import heroimg from '../assets/hero.png';
 import homeleft from '../assets/Home left image.jpg';
+import homeVideo from '../assets/Section.mp4';
 
-const homeVideo = '/Home.mp4';
+
 const Home = () => {
   return (
     <div>
